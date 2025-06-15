6315492e22ab7a11fc5ac3acc97c9c3c7e29f895
gslim <gslim@gslimui-MacBookAir.local>
2025-06-15 14:13:46 +0900

    Add initial terraform files

--- terraform/init/gslim-id/init.tf
+++ terraform/init/gslim-id/init.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "tfstate" {
-  bucket = "zerone-id-apnortheast2-tfstate"
+  bucket = "gslim-id-apnortheast2-tfstate"
 }
 
 resource "aws_s3_bucket_versioning" "versioning" {
